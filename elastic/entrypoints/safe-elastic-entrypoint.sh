e0500edb2457ec86e13d18d4765b0dfa2f523449
Cioclea Doru <doru.cioclea@corebuild.eu>
2020-07-02 11:53:07 +0300

    chore(elastic): Make sure elastic starts as supposed

--- elastic/entrypoints/safe-elastic-entrypoint.sh
+++ elastic/entrypoints/safe-elastic-entrypoint.sh
@@ -2,7 +2,12 @@
 
 if [[ $RUNNING_MODE == "elasticsearch" || $RUNNING_MODE == "master" || $RUNNING_MODE == "ingest" || $RUNNING_MODE == "data" || $RUNNING_MODE == "all" ]]; 
 then 
-    /elastic-entrypoint.sh $RUNNING_MODE
+    if [[ $RUNNING_MODE == "all" ]];
+    then
+        /elastic-entrypoint.sh elasticsearch;
+    else
+        /elastic-entrypoint.sh $RUNNING_MODE;
+    fi
 else 
     echo "Not starting the elasticsearch service as command is not any of (elasticsearch, master, data, ingest or all)"; 
 fi
